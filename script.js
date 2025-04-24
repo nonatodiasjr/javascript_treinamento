db5ca6ad17f53d7ef9e98887ccb571f5c7c2d87f
nonatodiasjr <jrdias.dev@gmail.com>
2025-04-24 11:40:23 -0300

    string, number, booleans

---
+++ script.js
@@ -0,0 +1,44 @@
+// utilizar const e let
+// string = texto
+
+/*const firstName = 'Lucas'
+const lastName = 'Lima'
+const age = 25
+
+const names = 'Lucas, Felipe, João, Maria, Ana, Pedro'
+
+console.log('Nome: ' + firstName + ' ' + lastName)
+console.log(`Nome: ${firstName} ${lastName}`)
+console.log(`Idade: ${age}`)
+console.log(
+  `Nome: ${firstName.toUpperCase()} ${lastName.toLowerCase()} Idade: ${age}`
+)
+
+console.log(names.split('')) // ['Lucas', 'Felipe', 'João', 'Maria', 'Ana', 'Pedro']*/
+
+//number
+/*
+const number = 10;
+const number2 = 5;
+soma = (a, b) => a + b
+console.log(number.toString()) // '10'
+console.log(typeof number.toString()) // 'string'
+console.log(soma(number,number2)) // 15
+
+*/
+
+//booleans
+/*
+console.log(2 == 2) // true
+console.log(2 == '2') // true
+console.log(2 === '2') // false
+console.log(2 != 2) // false
+console.log(2 != '2') // false
+console.log(2 !== '2') // true
+console.log(2 > 2) // false
+console.log(2 < 2) // false
+console.log(2 >= 2) // true
+console.log(2 <= 2) // true
+console.log(2 > 3) // false
+console.log(2 < 3) // true
+*/
